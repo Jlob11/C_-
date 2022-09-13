fef09224ff1844efeb6b1a3dc7db489c7279f343
Vel <alexei987654321@mail.ru>
2022-09-13 21:18:04 +0300

    New random number

--- Hello_world_001/Example003_Summation/Program.cs
+++ Hello_world_001/Example003_Summation/Program.cs
@@ -1,3 +1,5 @@
-﻿int number1 = 69;
-int number2 = 5;
-Console.WriteLine(number1 + number2);
+﻿int numberA = new Random() .Next(1, 10); // случайное число 123456789
+Console.WriteLine(numberA);
+int numberB = new Random() .Next(1, 10); // случайное число 123456789 
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
